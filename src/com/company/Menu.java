48e4f5231cd4c5031b04ca228967b2c12e6addf0
Kasturez <55916076+Kasturez@users.noreply.github.com>
2020-08-27 21:32:32 +0700

    displayall lead

--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Menu {
     public static void start() {
-        Lead lead = Lead.example;
-        Interaction interaction = Interaction.example;
         Database leadDatabase = new Database(Lead.fileName, Lead.idPrefix);
         Database interactionDatabase = new Database(Interaction.fileName, Interaction.idPrefix);
+        String[] leads = leadDatabase.getAll();
+        String[] interactions = interactionDatabase.getAll();
         String leftAlignFormat = "| %-15s | %-6s |%n";
-        String  displayAllFormat = "| %-23s |%n";
+        String  displayAllFormat = "| %-7s | %-8s | %-14s | %-6s | %-13s | %-17s | %-23s |%n";
         System.out.format("+-----------------+--------+%n");
         System.out.format("| Access          | Inputs |%n");
         System.out.format("+-----------------+--------+%n");
@@ -21,7 +21,7 @@ public class Menu {
 
         String input = sc.nextLine();
         switch (input) {
-            case "1":
+            case "1"://lead
                 System.out.format("+-----------------+--------+%n");
                 System.out.format("| Tasks           | Inputs |%n");
                 System.out.format("+-----------------+--------+%n");
@@ -31,15 +31,15 @@ public class Menu {
                 System.out.format("+-----------------+--------+%n");
                 String input1 = sc.nextLine();
                 switch (input1) {
-                    case "1":
-                        System.out.format("+-----------------+--------+%n");
-                        System.out.format("| Tasks           | Inputs |%n");
-                        System.out.format("+-----------------+--------+%n");
-                        for (String leadString: leadDatabase.getAll()
-                             ) {
-                            System.out.format(displayAllFormat,leadString);
+                    case "1"://lead display all
+                        System.out.format("+----------+----------+------------------------------+--------+---------------+-------------------+-------------------------+%n");
+                        System.out.format("| Lead ID  |   Name   |          Birth Date          | Gender |     Phone     |       Email       |         Address         |%n");
+                        System.out.format("+----------+----------+------------------------------+--------+---------------+-------------------+-------------------------+%n");
+                        for (int i = 0; i < leads.length; i++) {
+                            Lead lead = Lead.fromCSV(leads[i]);
+                            System.out.format(displayAllFormat,lead.getId(),lead.getName(),lead.getBirthDate(),lead.isMale(),lead.getPhone(),lead.getEmail(),lead.getAddress());
                         }
-                        System.out.format("+-----------------+--------+%n");
+                        System.out.format("+----------+----------+------------------------------+--------+---------------+-------------------+-------------------------+%n");
 
                         break;
                     case "2":
@@ -52,7 +52,7 @@ public class Menu {
                         break;
                 }
                 break;
-            case "2":
+            case "2"://interaction
                 System.out.format("+-----------------+--------+%n");
                 System.out.format("| Tasks           | Inputs |%n");
                 System.out.format("+-----------------+--------+%n");
@@ -63,7 +63,10 @@ public class Menu {
                 String input2 = sc.nextLine();
                 switch (input2) {
                     case "1":
-                        interactionDatabase.getAll();
+                        for (int i = 0; i < interactions.length; i++) {
+                            Interaction interaction = Interaction.fromCSV(interactions[i]);
+                            System.out.println(interaction.toString());
+                        }
                         break;
                     case "2":
                         System.out.println("which id are you looking for?");
