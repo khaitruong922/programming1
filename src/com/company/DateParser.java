e4f8c3bf99f7ddd5bb176e26cac70a69de47e2ed
Khai Truong <kd.kidr@gmail.com>
2020-08-27 17:13:34 +0700

    Parse CSV finished

---
+++ src/com/company/DateParser.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateParser {
+    private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+    public static Date stringToDate(String s) throws ParseException {
+        return sdf.parse(s);
+    }
+
+    public static String dateToString(Date d) {
+        if (d == null) return "";
+        return sdf.format(d);
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,19 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Hello World");
-        Menu.start();
+        Database leadDatabase = new Database(Lead.fileName,Lead.idPrefix);
+        Database interactionDatebase = new Database(Interaction.fileName,Interaction.idPrefix);
+        String[] leads = leadDatabase.getAll();
+        String[] interactions = interactionDatebase.getAll();
+        System.out.println("All leads: \n");
+        for (int i = 0; i < leads.length; i++) {
+            Lead lead = Lead.fromCSV(leads[i]);
+            System.out.println(lead.toString());
+        }
+        System.out.println("All interactions: \n");
+        for (int i = 0; i < interactions.length; i++) {
+            Interaction interaction = Interaction.fromCSV(interactions[i]);
+            System.out.println(interaction.toString());
+        }
     }
 }
