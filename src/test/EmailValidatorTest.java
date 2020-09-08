cea4ce2508254249795d886abe135f67f1d878e8
Khai Truong <kd.kidr@gmail.com>
2020-09-08 20:15:31 +0700

    add more email validation test

--- src/test/EmailValidatorTest.java
+++ src/test/EmailValidatorTest.java
@@ -6,5 +6,11 @@ public class EmailValidatorTest {
     public static void main(String[] args) {
         EmailValidator emailValidator = new EmailValidator();
         System.out.println(emailValidator.validate("huynh@gmail.com"));
+        System.out.println(emailValidator.validate(""));
+        System.out.println(emailValidator.validate("@"));
+        System.out.println(emailValidator.validate("abc@"));
+        System.out.println(emailValidator.validate("abc@a"));
+        System.out.println(emailValidator.validate("abc@a."));
+        System.out.println(emailValidator.validate("abc@b.."));
     }
 }
